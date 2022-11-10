4e252b431642b59d20edaacb14c0ff49ceaa67a9
lingxuan <lingxuan0618@gmail.com>
2022-11-10 22:15:02 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import popper from '@popperjs/core';
+import 'bootstrap';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+window.Popper = popper;
+
+createApp(App).use(store).use(router).mount('#app');
